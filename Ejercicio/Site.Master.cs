3eeb530951cb26c9dadcd336c2b1edf59e20f132
Nicolás <nicolascaytuirosilva@gmail.com>
2021-09-05 23:21:54 -0500

    Se culmino con la implementacion del ejercicio propuesto en la practica de laboratorio

---
+++ Ejercicio/Site.Master.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ejercicio
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (this.IsPostBack == false) 
+            {
+                // Productos
+                tipop.Items.Add(new ListItem("Cereales", "1"));
+                tipop.Items.Add(new ListItem("Menestras", "2"));
+                tipop.Items.Add(new ListItem("Carne de res", "3"));
+                tipop.Items.Add(new ListItem("Pollo", "4"));
+
+                // Nombre Empresas
+                nombreEmpresa.Items.Add(new ListItem("Gloria", "1"));
+                nombreEmpresa.Items.Add(new ListItem("Rico Pollo", "2"));
+                nombreEmpresa.Items.Add(new ListItem("Tottus", "3"));
+                nombreEmpresa.Items.Add(new ListItem("Promart", "4"));
+
+                productos.Visible = true;
+                clientes.Visible = false;
+                proveedor.Visible = false;
+            }
+        }
+
+        protected void mostrarFromProductos(Object sender, EventArgs e)
+        {
+            productos.Visible = true;
+            clientes.Visible = false;
+            proveedor.Visible = false;
+        }
+
+        protected void mostrarFromClientes(Object sender, EventArgs e)
+        {
+            productos.Visible = false;
+            clientes.Visible = true;
+            proveedor.Visible = false;
+        }
+
+        protected void mostrarFromProveedores(Object sender, EventArgs e)
+        {
+            productos.Visible = false;
+            clientes.Visible = false;
+            proveedor.Visible = true;
+        }
+
+        protected void crearProducto(Object sender, EventArgs e)
+        {
+            ListItem productos = tipop.Items[tipop.SelectedIndex];
+
+            contenedorRes.InnerHtml =
+                    "<h2>Ha ingresado los siguientes datos:</h2>" +
+                    "<p> 1. " + nombrep.Value + "</p>" +
+                    "<p> 2. " + productos.Text + "</p>" +
+                    "<p> 3. " + descripcionp.Value + "</p>" +
+                    "<p> 4. " + cantidadp.Value + "</p>" +
+                    "<p> 5. " + preciop.Value + "</p>";
+        }
+
+        protected void crearClientes(Object sender, EventArgs e)
+        {
+            contenedorRes.InnerHtml =
+                    "<h2>Ha ingresado los siguientes datos:</h2>" +
+                    "<p> 1. " + nombresc.Value + "</p>" +
+                    "<p> 2. " + apellidoc.Value + "</p>" +
+                    "<p> 3. " + telefonoc.Value + "</p>" +
+                    "<p> 4. " + mailc.Value + "</p>";
+        }
+
+        protected void crearProveedor(Object sender, EventArgs e)
+        {
+            ListItem proveedor = nombreEmpresa.Items[nombreEmpresa.SelectedIndex];
+
+            contenedorRes.InnerHtml =
+                    "<h2>Ha ingresado los siguientes datos:</h2>" +
+                    "<p> 1. " + nombrespr.Value + "</p>" +
+                    "<p> 2. " + apellidopr.Value + "</p>" +
+                    "<p> 3. " + telefonopr.Value + "</p>" +
+                    "<p> 4. " + proveedor.Text + "</p>" +
+                    "<p> 5. " + direccionpr.Value + "</p>";
+        }
+    }
+}
---
+++ Ejercicio/Site.Master.designer.cs
@@ -0,0 +1,179 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Ejercicio
+{
+
+
+    public partial class SiteMaster
+    {
+
+        /// <summary>
+        /// Control productos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl productos;
+
+        /// <summary>
+        /// Control nombrep.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText nombrep;
+
+        /// <summary>
+        /// Control tipop.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList tipop;
+
+        /// <summary>
+        /// Control descripcionp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText descripcionp;
+
+        /// <summary>
+        /// Control cantidadp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText cantidadp;
+
+        /// <summary>
+        /// Control preciop.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText preciop;
+
+        /// <summary>
+        /// Control clientes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl clientes;
+
+        /// <summary>
+        /// Control nombresc.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText nombresc;
+
+        /// <summary>
+        /// Control apellidoc.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText apellidoc;
+
+        /// <summary>
+        /// Control telefonoc.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText telefonoc;
+
+        /// <summary>
+        /// Control mailc.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl mailc;
+
+        /// <summary>
+        /// Control proveedor.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl proveedor;
+
+        /// <summary>
+        /// Control nombrespr.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText nombrespr;
+
+        /// <summary>
+        /// Control apellidopr.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText apellidopr;
+
+        /// <summary>
+        /// Control telefonopr.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText telefonopr;
+
+        /// <summary>
+        /// Control nombreEmpresa.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList nombreEmpresa;
+
+        /// <summary>
+        /// Control direccionpr.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText direccionpr;
+
+        /// <summary>
+        /// Control contenedorRes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl contenedorRes;
+    }
+}
